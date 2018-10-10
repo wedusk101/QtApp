cd24d90b164da89233f360fcd3a155462e769586
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2018-10-10 14:45:08 +0530

    Qt Desktop App

---
+++ HelloWorld/mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->pushButton, SIGNAL(released()), this, SLOT(buttonPressed()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::buttonPressed()
+{
+    qDebug() << "Hello, world!";
+}
